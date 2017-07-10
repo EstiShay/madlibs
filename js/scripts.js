a472cf5a34c4116daad05c75a2a1383e5bd9d773
Bart Webber and Esti Shay <bart@>
2017-07-10 11:06:09 -0700

    refactor code to use forEach on an array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,31 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var fearExclamationInput = $("input#fear_exclamation").val();
-    var adjInput = $("input#adjective").val();
-    var nounInput = $("input#noun").val();
-    var otherNounInput = $("input#other_noun").val();
-    var weaponInput = $("input#weapon").val();
+    var blanks = ["person1", "person2", "animal", "exclamation", "fear_exclamation", "adjective", "noun", "other_noun", "weapon"];
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".fear_exclamation").text(fearExclamationInput);
-    $(".adjective").text(adjInput);
-    $(".noun").text(nounInput);
-    $(".other_noun").text(otherNounInput);
-    $(".weapon").text(weaponInput);
+    blanks.forEach(function(blank) {
+      var userInput = $("input#" + blank).val();
+      $("." + blank).text(userInput)
+    });
+
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var animalInput= $("input#animal").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var fearExclamationInput = $("input#fear_exclamation").val();
+    // var adjInput = $("input#adjective").val();
+    // var nounInput = $("input#noun").val();
+    // var otherNounInput = $("input#other_noun").val();
+    // var weaponInput = $("input#weapon").val();
+    //
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".fear_exclamation").text(fearExclamationInput);
+    // $(".adjective").text(adjInput);
+    // $(".noun").text(nounInput);
+    // $(".other_noun").text(otherNounInput);
+    // $(".weapon").text(weaponInput);
 
     $("#story").show();
 
